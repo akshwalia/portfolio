409f791f07a9a4922941060458a42034e65fd7b9
akshwalia <akshwalia20@gmail.com>
2025-02-03 20:36:20 +0530

    Reduced tooltip hover duration

--- app/components/tech-icon.js
+++ app/components/tech-icon.js
@@ -1,34 +1,39 @@
-"use client"
+"use client";
 
-import Image from 'next/image'
-import { motion } from 'framer-motion'
+import Image from "next/image";
+import { motion } from "framer-motion";
 import {
-    Tooltip,
-    TooltipContent,
-    TooltipProvider,
-    TooltipTrigger,
-} from "@/components/ui/tooltip"
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
-export function TechIcon({ src, alt, delay, className = '' }) {
-    return (
-        <TooltipProvider>
-            <Tooltip>
-                <TooltipTrigger asChild>
-                    <motion.div
-                        className="bubble rounded-full bg-primary-green w-12 sm:w-28 h-12 sm:h-28 flex flex-col items-center justify-center"
-                        initial={{ y: -10, opacity: 0 }}
-                        whileInView={{ y: 0, opacity: 1 }}
-                        viewport={{ once: true, amount: 0.5 }}
-                        transition={{ duration: 0.4, ease: 'easeInOut', delay }}
-                    >
-                        <Image src={src} width={70} height={70} alt={alt} className={`w-[27px] sm:w-[70px] ${className}`} />
-                    </motion.div>
-                </TooltipTrigger>
-                <TooltipContent className="bg-white/80 text-xs z-50 text-primary-green border-2 border-primary-green rounded-full font-bold">
-                    <p>{alt}</p>
-                </TooltipContent>
-            </Tooltip>
-        </TooltipProvider>
-    )
+export function TechIcon({ src, alt, delay, className = "" }) {
+  return (
+    <TooltipProvider delayDuration={300}>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <motion.div
+            className="bubble rounded-full bg-primary-green w-12 sm:w-28 h-12 sm:h-28 flex flex-col items-center justify-center"
+            initial={{ y: -10, opacity: 0 }}
+            whileInView={{ y: 0, opacity: 1 }}
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ duration: 0.4, ease: "easeInOut", delay }}
+          >
+            <Image
+              src={src}
+              width={70}
+              height={70}
+              alt={alt}
+              className={`w-[27px] sm:w-[70px] ${className}`}
+            />
+          </motion.div>
+        </TooltipTrigger>
+        <TooltipContent className="bg-white/80 text-xs z-50 text-primary-green border-2 border-primary-green rounded-full font-bold">
+          <p>{alt}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
 }
-
